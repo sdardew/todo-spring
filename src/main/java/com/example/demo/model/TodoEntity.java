21803e7d5bf3dfdd9513684847c5c2892d664488
sdardew <sdardew@gmail.com>
2021-12-15 14:36:19 +0900

    Setting to use jpa

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -4,12 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity // 엔티티로 지정
+@Table(name = "Todo") // 테이블 이름 이름 지정
 public class TodoEntity {
+    @Id //기본키가 될 필드
+    @GeneratedValue(generator="system-uuid") // id를 자동으로 생성. generator -> id를 생성하는 방식
+    @GenericGenerator(name="system-uuid", strategy="uuid") // uuid를 사용하는 system-uuid라는 이름의 generator 생성
     private String id; // 오브젝트 아이디
     private String userId; // 오브젝트를 생성한 사용자의 아이디
     private String title; // Todo 타이틀
