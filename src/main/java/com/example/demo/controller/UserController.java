38801198ad16d74f2820eb0024a84aec42c3def9
sdardew <sdardew@gmail.com>
2022-01-13 16:56:33 +0900

    Add jwt token

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,9 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.model.UserEntity;
+import com.example.demo.security.TokenProvider;
 import com.example.demo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +20,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TokenProvider tokenProvider;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
@@ -52,9 +55,11 @@ public class UserController {
                 userDTO.getPassword());
 
         if (user != null) {
+            final String token = tokenProvider.create(user);
             final UserDTO responseUserDTO = UserDTO.builder()
                     .email(user.getEmail())
                     .id(user.getId())
+                    .token(token)
                     .build();
 
             return ResponseEntity.ok().body(responseUserDTO);
---
+++ src/main/java/com/example/demo/security/TokenProvider.java
@@ -0,0 +1,41 @@
+package com.example.demo.security;
+
+import com.example.demo.model.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Slf4j
+@Service
+public class TokenProvider {
+    private static final String SECRET_KEY = "SecRetkey1secretKey2";
+
+    // token 생성
+    public String create(UserEntity userEntity) {
+        Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.DAYS)); // 7 days
+
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .setSubject(userEntity.getId()) // sub
+                .setIssuer("demo app") // iss
+                .setIssuedAt(new Date()) // iat
+                .setExpiration(expiryDate) // exp
+                .compact();
+    }
+
+    // 토큰 디코딩 & 파싱 & 검사
+    public String validateAndGetUserId(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
